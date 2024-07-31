f30e40e7758397fd0693ba41b27f9f51e920f6d8
Sasu Catalin-Gabriel <sasucatalingabriel@gmail.com>
2024-07-31 11:06:26 +0300

    Local fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ChatbotComponent } from './features/chatbot/chatbot.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/chatbot', pathMatch: 'full' },
+  {path: 'chatbot', component: ChatbotComponent},
+];
